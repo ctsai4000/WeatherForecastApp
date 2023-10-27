2d66039f6b6ce3958d3643379909de7e0bc23ce3
Lawrence <consultant@MBP-C02ZT45VMD6M-TESCHROEDER.local>
2023-10-26 21:54:17 -0400

    init project

---
+++ WeatherForecastApp/WeatherForecastApp/WeatherForecastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastAppApp.swift
+//  WeatherForecastApp
+//
+//  Created by Lawrence on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
